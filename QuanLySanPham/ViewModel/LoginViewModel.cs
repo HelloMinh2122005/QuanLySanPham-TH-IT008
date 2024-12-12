5a9d9a72cdbea0904af977cc523baf4ec8fd5a60
helloMinh <phandinhminh48@gmail.com>
2024-12-12 22:40:10 +0700

    add login

--- QuanLySanPham/ViewModel/LoginViewModel.cs
+++ QuanLySanPham/ViewModel/LoginViewModel.cs
@@ -6,12 +6,9 @@ namespace QuanLySanPham.ViewModel;
 public partial class LoginViewModel : ObservableObject
 {
     [ObservableProperty]
-    public string username;
-
-    public LoginViewModel()
-    {
-        Username = "";
-    }
+    public string username = "";
+    [ObservableProperty]
+    public string password = "";
 
     [RelayCommand]
     async Task Continue()
@@ -19,6 +16,16 @@ public partial class LoginViewModel : ObservableObject
         if (Username == "")
         {
             await Shell.Current.DisplayAlert("Thông báo", "Vui lòng nhập tên đăng nhập", "OK");
+            return;
+        }
+        if (Password == "")
+        {
+            await Shell.Current.DisplayAlert("Thông báo", "Vui lòng nhập mật khẩu", "OK");
+            return;
+        }
+        if (Username != "Phan Dinh Minh" || Password != "minh2005")
+        {
+            await Shell.Current.DisplayAlert("Thông báo", "Tên đăng nhập hoặc mật khẩu không đúng", "OK");
             return;
         }
         await Shell.Current.GoToAsync(nameof(MainPage), new Dictionary<string, object>
